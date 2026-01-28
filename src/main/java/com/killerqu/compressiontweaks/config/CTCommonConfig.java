e9b9c78c33b8e1b11c6e57cd0b781ac0db85461d
Big Barza <francesco.b99@hotmail.it>
2026-01-28 12:19:13 +0100

    Configs for vibrating table internal capacity

--- src/main/java/com/killerqu/compressiontweaks/config/CTCommonConfig.java
+++ src/main/java/com/killerqu/compressiontweaks/config/CTCommonConfig.java
@@ -13,6 +13,7 @@ public class CTCommonConfig {
     public static final ForgeConfigSpec.ConfigValue<Integer> CLOUD_BOTTLING_MIN_Y;
     public static final ForgeConfigSpec.ConfigValue<Integer> CLOUD_BOTTLING_MAX_Y;
     public static final ForgeConfigSpec.ConfigValue<Integer> COMPRESSOR_FLUID_CAPACITY;
+    public static final ForgeConfigSpec.ConfigValue<Integer> TABLE_OUTPUT_SLOTS;
     public static final ForgeConfigSpec.ConfigValue<List<? extends String>> EMI_BLACKLIST;
 
     static {
@@ -26,6 +27,8 @@ public class CTCommonConfig {
                 .define("Cloud in a Bottle Maximum Y-Level", 196);
         COMPRESSOR_FLUID_CAPACITY = BUILDER.comment("The capacity of the internal tanks for the Vintage Improvements compressor")
                 .define("VI Compressor Fluid Capacity", 1000);
+        TABLE_OUTPUT_SLOTS = BUILDER.comment("The amount of output slots in the Vintage Improvements vibrating table")
+                .define("VI Vibrating Table Output Slots", 9);
         EMI_BLACKLIST = BUILDER.comment("A list of EMI recipe IDs to be blacklisted. Should be used for hardcoded integrations.")
                         .defineList("EMI Recipe Blacklist", List.of(), o -> o instanceof String s && ResourceLocation.isValidResourceLocation(s));
 
